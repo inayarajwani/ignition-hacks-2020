cbe144f9ad389ad465defaec413512c42e87460c
HasanMajid <70066549+HasanMajid@users.noreply.github.com>
2020-08-23 14:03:35 -0400

    Update textextractor.py

--- textextractor.py
+++ textextractor.py
@@ -1,18 +1,26 @@
+sentences = []
 words = []
-txt = ""
+
+#Name and location of the image
 fileDir='c:\Awesome.png'
 import pytesseract
 
-print("hello world!")
-
-
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+def extract(x):
+    #splits the sentence into individual words
+    x = (pytesseract.image_to_string(fileDir)).split()
+    #Adds the sentence to the list as a whole sentence
+    sentences.append(pytesseract.image_to_string(fileDir))
+    return x
 
-txt = (pytesseract.image_to_string(fileDir)).split()
-words.append(pytesseract.image_to_string(fileDir))
+  
+words = extract(fileDir)
 
-print(pytesseract.image_to_string(fileDir))
-print(txt[0])
+#print(pytesseract.image_to_string(fileDir))
 
-return txt
+#prints a whole sentence
+print (sentences[0])
+
+#prints individual words
+print(words[0])
