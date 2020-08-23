c3bc00bda66e100a95b15e14dbde5d314ea47e47
inayarajwani <47422650+inayarajwani@users.noreply.github.com>
2020-08-22 23:52:46 -0400

    Update ignitionhack_2020.py

--- ignitionhack_2020.py
+++ ignitionhack_2020.py
@@ -2,7 +2,6 @@ import textextractor.py
 
 def highlight_text(desired_text):
     right_word = []
-    #texts = ["apple", "cucumber", "stylist", "cucumber", "hand"]
     for word in txt:
         if desired_text == word:
             right_word.append(word)
@@ -11,4 +10,3 @@ def highlight_text(desired_text):
     print(str(right_word))
 
 
-#highlight_text("cucumber")
