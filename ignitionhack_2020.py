2eb0c7797b8ad47f0a59aff0ee47aac433a445d8
inayarajwani <47422650+inayarajwani@users.noreply.github.com>
2020-08-22 23:50:49 -0400

    text prep 1

---
+++ ignitionhack_2020.py
@@ -0,0 +1,14 @@
+import textextractor.py
+
+def highlight_text(desired_text):
+    right_word = []
+    #texts = ["apple", "cucumber", "stylist", "cucumber", "hand"]
+    for word in txt:
+        if desired_text == word:
+            right_word.append(word)
+        else:
+            continue
+    print(str(right_word))
+
+
+#highlight_text("cucumber")
