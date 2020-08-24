2ab302fa222c34350073e9a2626e818213f9b44e
inayarajwani <47422650+inayarajwani@users.noreply.github.com>
2020-08-23 20:53:37 -0400

    Update ignitionhack_2020.py

--- ignitionhack_2020.py
+++ ignitionhack_2020.py
@@ -2,7 +2,7 @@ import textextractor.py
 
 def highlight_text(desired_text):
     right_word = []
-    for word in txt:
+    for word in x:
         if desired_text == word:
             right_word.append(word)
         else:
