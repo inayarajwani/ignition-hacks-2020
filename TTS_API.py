8a621bcc6599ccdac4296a9cc4073d72e7571070
Vanessa L <70068643+xenoastra@users.noreply.github.com>
2020-08-23 23:23:02 -0400

    Update TTS_API.py

--- TTS_API.py
+++ TTS_API.py
@@ -1,7 +1,7 @@
 from gtts import gTTS
 import os
 import ignitionhack_2020
-import 
+import pytesseract
 tts=gTTS(text="awesome",lang="en") #hilights awesome
 tts.save("awesome.mp3")
 os.system("start awesome.mp3")
